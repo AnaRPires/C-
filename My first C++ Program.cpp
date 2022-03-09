f51751cb35b5344c49a7f8ee271f2e2f807c0363
Ana Pires <93409353+AnaRPires@users.noreply.github.com>
2022-03-09 17:32:57 +0000

    Add files via upload

---
+++ My first C++ Program.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+//using namespace gets rid of the ::<<
+using namespace std;
+//output
+//define a variable
+int main() {
+  cout << "Hello World!\n";
+  cout << "How are you doing??\n";
+    int myage=21;
+    cout <<"My age is " <<myage;
+    cout <<"\nMy age next year will be "<<myage+1;
+
+int yourage;
+  cout <<"\nWhat is your age?? ";
+//input use >>
+  cin >>yourage;
+  cout <<"\nYour age is "<<yourage; 
+  cout <<"\nYour age next year will be "<<yourage+1; 
+
+//Define a variable
+string name;
+cout <<"\nWhat is your name?? ";
+cin >>name;
+cout <<"\nHello "<<name<<"How are you today? ";   
+} 
